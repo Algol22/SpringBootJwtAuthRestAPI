69b1394a79de716faf87d6829ca4129aaa6eeb3b
lifelongeducation <andriyslb@gmail.com>
2023-05-21 15:47:41 +0300

    init

---
+++ src/main/java/com/rest_api/demo/User/Role.java
@@ -0,0 +1,6 @@
+package com.rest_api.demo.User;
+
+public enum Role {
+
+    USER, ADMIN
+}
---
+++ src/main/java/com/rest_api/demo/auth/AuthenticationRequest.java
@@ -0,0 +1,20 @@
+package com.rest_api.demo.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+}
